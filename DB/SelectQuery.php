5a87f0bc1c1a2578de7ea1b898012b5dd08f9d22
Rupendra Kumar Dhiman <Dhiman.RupendraK@hutchisonports.com>
2025-04-29 12:37:18 +0800

    Change based on DBwq

---
+++ DB/SelectQuery.php
@@ -0,0 +1,64 @@
+<?php
+
+require 'DBConnection.php';
+require 'SiteConfig.php';
+
+$dbConfig = DBConfig();
+
+class SelectQuery {
+  private $conn;
+  private $query;
+
+  public function __construct() {
+    $this->conn = DBConnection::getDbConn();
+  }
+
+  public function query($query) {
+    $this->query = $query;
+  }
+
+  public function selectAll() {
+    $stmt = $this->conn->prepare($this->query);
+    $output = $stmt->execute();
+
+    return $output ? $stmt->fetchAll(PDO::FETCH_ASSOC) : array();
+  }
+
+  public function selectByWhere($where) {
+    $stmt = $this->conn->prepare($this->query . ' where ' . $where);
+    $output = $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+}
+
+function makeSiteConfigData($row): SiteConfig {
+  if(!$row) throw new Exception('No data');
+
+  $siteConfig = new SiteConfig($row['id'], $row['route'], $row['title'], $row['description'], $row['keywords']);
+  $siteConfig->setOgData($row['ogSiteName'], $row['ogUrl'], $row['ogTitle'], $row['ogDescription'], $row['ogType'], $row['ogSeeAlso'], $row['ogLocale'], $row['ogLocaleAlternate1'], $row['ogLocaleAlternate2'], $row['ogUpdatedTime']);
+  $siteConfig->setOgImageData($row['ogImageType'], $row['ogImageWidth'], $row['ogImageHeight'], $row['ogImageUrl'], $row['ogImageSecureUrl'], $row['ogImageAlt']);
+  $siteConfig->setOgVideoData($row['ogVideoType'], $row['ogVideoWidth'], $row['ogVideoHeight'], $row['ogVideoSecureUrl']);
+
+  return $siteConfig;
+}
+
+function getSiteConfig($route = "") {
+  $select = new SelectQuery();
+  $select->query("SELECT * FROM seo_setting");
+
+  if($route === "") {
+    $result = $select->selectAll();
+
+    $siteConfigs = array();
+    foreach($result as $k=>$v) {
+      array_push($siteConfigs, makeSiteConfigData($v));
+    }
+    
+    return $siteConfigs;
+  } else {
+    $result = $select->selectByWhere("route = '".$route."' ");
+
+    return makeSiteConfigData($result);
+  }
+}
--- metaTag.php
+++ metaTag.php
@@ -1,4 +1,11 @@
-<?php include './configuration.php'; ?>
+<?php 
+  include './DB/SelectQuery.php';
+  $rquestUri = $_SERVER['REQUEST_URI'] === '/' ? '/home_interior_design_in_noida' : $_SERVER['REQUEST_URI'];
+  $rquestUri = str_replace('/pw_initial/metaTag.php', '', $rquestUri);
+  $seo = getSiteConfig($rquestUri);
+  // echo '<pre>';print_r($seo);echo '</pre>';
+?>
+<title><?php echo $seo->getTitle(); ?></title>
 
 <link rel="icon" href="./favicon.png" />
 <meta name="language" content="English" />
@@ -6,44 +13,42 @@
 <meta name="theme-color" content="#000000" />
 <meta name="google-site-verification" content="z_5HlCkpyxt7dVxFX-65bH-qVoqJc-b-Vvz0mED9j9k" />
 
-<!-- Regular Tags -->
-<title><?php echo $seo->getTitle(); ?></title>
 <meta http-equiv="content-language" content="en" />
 <meta data-hid="http-equiv" http-equiv="Accept-CH" content="DPR, Viewport-Width, Width, Save-Data" />
 <meta charset="utf-8" />
 <meta name="description" content="<?php echo $seo->getDescription(); ?>" />
 <meta name="keywords" content="<?php echo $seo->getKeywords(); ?>" />
 <!-- <link rel="author" href="https://plus.google.com/{{googlePlusId}}" /> -->
-<link rel="canonical" href="<?php echo $seo->getOG()->getSeeAlso(); ?>" />
+<link rel="canonical" href="<?php echo $seo->getOGSeeAlso(); ?>" />
 
 <!-- Faceboot Meta Tags -->
-<meta property="og:site_name" content="<?php echo $seo->getOG()->getSiteName(); ?>" />
-<meta property="og:url" content="<?php echo $seo->getOG()->getUrl(); ?>" />
-<meta property="og:title" content="<?php echo $seo->getOG()->getTitle(); ?>" />
-<meta property="og:see_also" content="<?php echo $seo->getOG()->getSeeAlso(); ?>" />
-<meta property="og:description" content="<?php echo $seo->getOG()->getDescription(); ?>" />
-<meta property="og:locale" content="<?php echo $seo->getOG()->getLocale(); ?>" />
-<meta property="og:locale:alternate" content="<?php echo $seo->getOG()->getLocaleAlternate1(); ?>" />
-<meta property="og:type" content="<?php echo $seo->getOG()->getType(); ?>" />
+<meta property="og:site_name" content="<?php echo $seo->getOGSiteName(); ?>" />
+<meta property="og:url" content="<?php echo $seo->getOGUrl(); ?>" />
+<meta property="og:title" content="<?php echo $seo->getOGTitle(); ?>" />
+<meta property="og:see_also" content="<?php echo $seo->getOGSeeAlso(); ?>" />
+<meta property="og:description" content="<?php echo $seo->getOGDescription(); ?>" />
+<meta property="og:locale" content="<?php echo $seo->getOGLocale(); ?>" />
+<meta property="og:locale:alternate" content="<?php echo $seo->getOGLocaleAlternate1(); ?>" />
+<meta property="og:type" content="<?php echo $seo->getOGType(); ?>" />
 
-<meta property="og:image" content="<?php echo $seo->getOGImage()->getUrl(); ?>" />
-<meta property="og:image:secure_url" content="<?php echo $seo->getOGImage()->getSecureUrl(); ?>" />
-<meta property="og:image:type" content="<?php echo $seo->getOGImage()->getType(); ?>" />
-<meta property="og:image:width" content="<?php echo $seo->getOGImage()->getWidth(); ?>" />
-<meta property="og:image:height" content="<?php echo $seo->getOGImage()->getHeight(); ?>" />
-<meta property="og:image:alt" content="<?php echo $seo->getOGImage()->getAlt(); ?>" />
+<meta property="og:image" content="<?php echo $seo->getOGImageUrl(); ?>" />
+<meta property="og:image:secure_url" content="<?php echo $seo->getOGImageSecureUrl(); ?>" />
+<meta property="og:image:type" content="<?php echo $seo->getOGImageType(); ?>" />
+<meta property="og:image:width" content="<?php echo $seo->getOGImageWidth(); ?>" />
+<meta property="og:image:height" content="<?php echo $seo->getOGImageHeight(); ?>" />
+<meta property="og:image:alt" content="<?php echo $seo->getOGImageAlt(); ?>" />
 
 <!-- Google Meta Tags -->
-<meta itemprop="name" content="<?php echo $seo->getOG()->getTitle(); ?>" />
-<meta itemprop="description" content="<?php echo $seo->getOG()->getDescription(); ?>" />
-<meta itemprop="image" content="<?php echo $seo->getOGImage()->getUrl(); ?>" />
+<meta itemprop="name" content="<?php echo $seo->getOGTitle(); ?>" />
+<meta itemprop="description" content="<?php echo $seo->getOGDescription(); ?>" />
+<meta itemprop="image" content="<?php echo $seo->getOGImageUrl(); ?>" />
 <!-- Google Meta Tags End -->
 
 <!-- Twitter Meta tags -->
 <meta name="twitter:card" content="summary" />
-<meta name="twitter:url" content="<?php echo $seo->getOG()->getSeeAlso(); ?>" />
-<meta name="twitter:title" content="<?php echo $seo->getOG()->getTitle(); ?>" />
-<meta name="twitter:description" content="<?php echo $seo->getOG()->getDescription(); ?>" />
+<meta name="twitter:url" content="<?php echo $seo->getOGSeeAlso(); ?>" />
+<meta name="twitter:title" content="<?php echo $seo->getOGTitle(); ?>" />
+<meta name="twitter:description" content="<?php echo $seo->getOGDescription(); ?>" />
 <!-- Twitter Meta tags End -->
 
 <!-- Geo Position Meta Tags -->
